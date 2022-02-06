639f6281a1fc88c3e87abefb483aefed796301a9
Yoshinori Nomura <yoshi@Yoshinoris-Air.home>
2022-02-06 18:58:44 -0500

    Updated Name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Yoshinori Nomura"
         return render_template('index.html', name=name)
